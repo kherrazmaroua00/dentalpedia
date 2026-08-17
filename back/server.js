3e76c6055d6b010d04eebc4fc7a58a2ebec1d06d
maroua <kh.mar0uua@gmail.com>
2026-08-18 00:30:47 +0100

    fix

--- back/server.js
+++ back/server.js
@@ -29,3 +29,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
